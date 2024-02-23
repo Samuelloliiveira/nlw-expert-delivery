c597338e2d9d554b5e661e95fcd9e9daa0bb6c65
Samuel Oliveira <samuelcontatodev@gmail.com>
2024-02-22 21:17:33 -0300

    salvando os dados no celular do usuário, enviando pedido para o whatsapp

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,7 +1,8 @@
-import { Alert, ScrollView, Text, View } from 'react-native'
-import { Feather } from '@expo/vector-icons'
-
+import { useState } from 'react'
+import { Alert, Linking, ScrollView, Text, View } from 'react-native'
+import { useNavigation } from 'expo-router'
 import { useCartStore } from '@/stores/cart-store'
+import { Feather } from '@expo/vector-icons'
 
 import { Header } from '@/components/header'
 import { Product } from '@/components/product'
@@ -10,11 +11,42 @@ import { Input } from '@/components/input'
 import { Button } from '@/components/button'
 import { LinkButton } from '@/components/link-button'
 
+const PHONE_NUMBER = "5571999040682"
+
 export default function Cart() {
+  const [address, setAddress] = useState("")
   const cartStore = useCartStore()
+  const navigation = useNavigation()
 
   const total = formatCurrency(cartStore.products.reduce((total, product) =>
     total + product.price * product.quantity, 0))
+
+  function handleOrder() {
+    if (address.trim().length === 0) {
+      return Alert.alert("Pedido", "Informe os dados da entrega!")
+    }
+
+    const products = cartStore.products
+      .map((product) => `\n ${product.quantity} ${product.title}`)
+      .join("")
+
+    const message = `
+      NOVO PEDIDO 😋
+      
+      \n Entregar em: ${address}
+
+      ${products}
+
+      \n Valor total: ${total}
+    `
+
+    Linking.openURL(`http://api.whatsapp.com/send?phone=${PHONE_NUMBER}&text=${message}`)
+
+    cartStore.clear()
+    setAddress("")
+
+    navigation.goBack()
+  }
 
   return (
     <View className="flex-1 pt-16">
@@ -39,10 +71,15 @@ export default function Cart() {
       <Input
         placeholder="Informe o endereço de endrega com rua, bairro, CEP, número e complemento."
         className="mx-5"
+        onChangeText={setAddress}
+        value={address}
+        blurOnSubmit={true}
+        onSubmitEditing={handleOrder}
+        returnKeyType="next"
       />
 
       <View className="p-5 gap-5">
-        <Button>
+        <Button onPress={handleOrder}>
           <Button.Text>Enviar pedido</Button.Text>
           <Button.Icon>
             <Feather name="arrow-right-circle" size={20} />
